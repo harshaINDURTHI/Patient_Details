853e69814f690a56943001ed78700c396f3c5c26
indurthi Harshavardhan <Harshavardhan.indurthi@healthfirsttech.local>
2025-04-17 14:31:28 +0530

    Second commit

--- src/main/java/com/example/postgredbconnect/PostgredbconnectApplication.java
+++ src/main/java/com/example/postgredbconnect/PostgredbconnectApplication.java
@@ -9,7 +9,7 @@ public class PostgredbconnectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PostgredbconnectApplication.class, args);
 		
-		System.out.println("hello harsha !!");
+		System.out.println("hello harsha !!" + "hello world");
 	}
 
 }
